adf327cf28901419d9bcd31632310c9125f261e3
Brandon Horst <brandonhorst@gmail.com>
2014-12-06 20:06:45 -0500

    Added some aliases

---
+++ aliases/shortcuts.zsh
@@ -0,0 +1,6 @@
+alias ll='ls -l'
+alias la='ls -la'
+
+alias gs='git status'
+alias ga='git add .'
+alias gc='git commit'
--- init.zsh
+++ init.zsh
@@ -1,3 +1,5 @@
 SOURCE=${0%/*}
 
 source $SOURCE/functions/functions.zsh
+
+source $SOURCE/aliases/shortcuts.zsh
