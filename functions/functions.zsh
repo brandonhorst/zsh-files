3dc6df1055b10ca3b7cbfd4993fb2a64cbb35fdb
Brandon Horst <brandonhorst@gmail.com>
2014-12-10 09:32:07 -0500

    Added github and npmjs commands

--- functions/functions.zsh
+++ functions/functions.zsh
@@ -22,3 +22,11 @@ function unblock {
   egrep -v "127\\.0\\.0\\.1\\s+$1" /etc/hosts > /tmp/tmphosts
   cat /tmp/tmphosts > /etc/hosts
 }
+
+function npmjs {
+  echo "https://npmjs.com/package/`npm view . name`" | xargs open
+}
+
+function github {
+  git config --get remote.origin.url | xargs open
+}
