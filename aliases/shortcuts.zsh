da46d2d5e3c0789e808dd630df66e76e2095649f
Brandon Horst <brandonhorst@gmail.com>
2015-01-02 15:51:23 -0500

    Added some shortcuts

--- aliases/shortcuts.zsh
+++ aliases/shortcuts.zsh
@@ -4,3 +4,5 @@ alias la='ls -la'
 alias gs='git status'
 alias ga='git add .'
 alias gc='git commit'
+
+eval "$(hub alias -s)"
---
+++ install/install.zsh
@@ -0,0 +1,6 @@
+brew install git
+brew install hub
+brew install sox
+brew install zsh
+brew install wget
+brew install node
